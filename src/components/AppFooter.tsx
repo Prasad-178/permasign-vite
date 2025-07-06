304a17e54237e35cea4e688ebab0d6adaa7ac369
Prasad-178 <prasadjs178@gmail.com>
2025-07-06 19:23:51 +0530

    chore: removed unused vars

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,15 +1,5 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
-// A simple placeholder SVG logo. You might want to reuse the one from Navbar.tsx
-// or extract it into its own component if you haven't already.
-const PermaSignLogo = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-primary">
-    <path d="M12 2L2 7L12 12L22 7L12 2Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    <path d="M2 17L12 22L22 17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    <path d="M2 12L12 17L22 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-  </svg>
-);
-
 type FooterLinkItem = {
     label: string;
     href: string;
